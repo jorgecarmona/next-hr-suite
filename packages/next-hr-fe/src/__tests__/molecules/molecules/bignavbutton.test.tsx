267da423f79418f8c90c0eb2c18fddda27b3f7d4
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2024-06-03 19:47:55 -0400

    feat: added BigNavButton fixes

--- packages/next-hr-fe/src/__tests__/molecules/molecules/bignavbutton.test.tsx
+++ packages/next-hr-fe/src/__tests__/molecules/molecules/bignavbutton.test.tsx
@@ -1,36 +1,39 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import BigNavButton from '../../molecules/bignavbutton';
-
-import { IconType } from '../../atoms/icon-store';
+import {BigNavButton} from '../../../molecules';
+import {IconType} from '../../../atoms/icon-store';
 
 describe('BigNavButton', () => {
-    it('renders without crashing', () => {
-        render(<BigNavButton 
-            bgColor = '#F0F9FF'
-            customColor = '#2E90FA'
-            icon = {IconType.Work}
-            onClickCallback = {() => {}}
-            />);
-    });
+  it('renders without crashing', () => {
+    render(
+      <BigNavButton
+        bgColor="#F0F9FF"
+        customColor="#2E90FA"
+        icon={IconType.Work}
+        onClickCallback={() => {}}
+      />,
+    );
+  });
 
-    it('calls onClickCallback when button is clicked', () => {
-        const mockHandleClick = jest.fn();
+  it('calls onClickCallback when button is clicked', () => {
+    const mockHandleClick = jest.fn();
 
-        render(<BigNavButton 
-            bgColor = '#F0F9FF'
-            customColor = '#2E90FA'
-            icon = {IconType.Work}
-            onClickCallback = {mockHandleClick}
-            subtitle = 'New Request'
-            title = 'Submit'
-            />);
+    render(
+      <BigNavButton
+        bgColor="#F0F9FF"
+        customColor="#2E90FA"
+        icon={IconType.Work}
+        onClickCallback={mockHandleClick}
+        subtitle="New Request"
+        title="Submit"
+      />,
+    );
 
-        const button = screen.getByText('Submit'); 
+    const button = screen.getByText('Submit');
 
-        userEvent.click(button); 
+    userEvent.click(button);
 
-        expect(mockHandleClick).toHaveBeenCalledTimes(1); 
-    });
+    expect(mockHandleClick).toHaveBeenCalledTimes(1);
+  });
 });
--- packages/next-hr-fe/src/molecules/bignavbutton.tsx
+++ packages/next-hr-fe/src/molecules/bignavbutton.tsx
@@ -1,35 +1,62 @@
-import { Button, Card as MuiCard, Icon, Typography as MuiTypography } from '../atoms'
-import { IconType } from '../atoms/icon-store';
+import {
+  Button,
+  Card as MuiCard,
+  Icon,
+  Typography as MuiTypography,
+} from '../atoms';
+import {IconType} from '../atoms/icon-store';
 
 import nextHrTheme from '../theme/theme';
 
 interface BigNavButtonProps {
   bgColor?: string;
-  customColor?: string; 
+  customColor?: string;
   icon: IconType;
   onClickCallback: (value: string) => void;
   subtitle?: string;
   title?: string;
 }
 
-export default function BigNavButton({bgColor, customColor, icon, onClickCallback, subtitle, title = ''}: BigNavButtonProps) {    
+export default function BigNavButton({
+  bgColor,
+  customColor,
+  icon,
+  onClickCallback,
+  subtitle,
+  title = '',
+}: BigNavButtonProps) {
   const buttonId = title.toLowerCase().replace(' ', '-');
   const layout = {width: '130px', height: '104px'};
-  const iconStyles = {style: {borderRadius: "50%", display: "inline-flex", alignItems: "center", justifyContent: "center", width: "48px", height: "48px"}};
+  const iconStyles = {
+    style: {
+      borderRadius: '50%',
+      display: 'inline-flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '48px',
+      height: '48px',
+    },
+  };
   const palette = nextHrTheme.palette.custom;
 
   return (
     <Button onClick={() => onClickCallback(buttonId)}>
-      <MuiCard bgColor={palette.paper}
+      <MuiCard
+        bgColor={palette.paper}
         content={
           <div style={layout}>
-            <Icon name={icon} {...iconStyles} bgColor={bgColor} customColor={customColor}/>
-            <MuiTypography variant='h5'>
+            <Icon
+              name={icon}
+              {...iconStyles}
+              bgColor={bgColor}
+              customColor={customColor}
+            />
+            <MuiTypography variant="h5">
               <span style={{color: palette.title, fontWeight: 600}}>
                 {title}
               </span>
             </MuiTypography>
-            <MuiTypography variant='h6'>
+            <MuiTypography variant="h6">
               <span style={{color: palette.subtitle, fontWeight: 400}}>
                 {subtitle}
               </span>
@@ -38,5 +65,5 @@ export default function BigNavButton({bgColor, customColor, icon, onClickCallbac
         }
       />
     </Button>
-  )
+  );
 }
