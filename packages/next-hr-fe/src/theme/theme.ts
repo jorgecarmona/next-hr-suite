fb5e0cdcadb41e11d96ff312675208046caa3102
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2024-06-03 18:56:48 -0400

    feat: migration of login page

--- packages/next-hr-fe/src/theme/theme.ts
+++ packages/next-hr-fe/src/theme/theme.ts
@@ -1,210 +1,208 @@
-import {createTheme} from '@mui/material';
+import {createTheme} from "@mui/material";
 const nextHrTheme = createTheme({
   palette: {
     primary: {
-      main: '#1570EF',
+      main: "#1570EF"
     },
     secondary: {
-      main: '#344054',
+      main: "#344054"
     },
     success: {
-      main: '#027A48',
+      main: "#027A48"
     },
     error: {
-      main: '#B42318',
+      main: "#B42318"
     },
     warning: {
-      main: '#FFA000',
+      main: "#FFA000"
     },
     custom: {
-      paper: '#FCFCFD',
-      title: '#1570EF',
-      subtitle: '#344054',
+      paper: "#FCFCFD"
     },
     info: {
-      main: '#FFFFFF',
-      light: '#F0F9FF',
-      contrastText: '#FFFFFF',
-    },
+      main: "#FFFFFF",
+      light: "#F0F9FF",
+      contrastText: "#FFFFFF"
+    }
   },
   typography: {
-    fontFamily: 'Poppins, sans-serif',
-    fontWeightMedium: 400,
+    fontFamily: "Poppins, sans-serif",
+    fontWeightMedium: 400
   },
   components: {
     MuiAvatar: {
       styleOverrides: {
         root: {
-          '&.profile': {
-            fontSize: '1rem',
+          "&.profile": {
+            fontSize: "1rem",
             fontWeight: 500,
-            backgroundColor: '#072136',
-            color: '#FFFFFF',
-          },
-        },
-      },
+            backgroundColor: "#072136",
+            color: "#FFFFFF"
+          }
+        }
+      }
     },
     MuiButton: {
       styleOverrides: {
         root: {
-          textTransform: 'none',
-          '&.tertiary': {
-            backgroundColor: '#F0F9FF',
-            color: '#1849A9',
-            '&:hover': {
-              backgroundColor: '#D0E7FF',
-            },
+          textTransform: "none",
+          "&.tertiary": {
+            backgroundColor: "#F0F9FF",
+            color: "#1849A9",
+            "&:hover": {
+              backgroundColor: "#D0E7FF"
+            }
           },
-          '&.selected': {
-            color: '#1849A9',
-            fontWeight: 500,
-          },
+          "&.selected": {
+            color: "#1849A9",
+            fontWeight: 500
+          }
         },
         outlined: {
-          backgroundColor: '#FFFFFF',
-          borderColor: '#D0D5DD',
-          color: '#344054',
-          '&:hover': {
-            backgroundColor: 'rgba(208, 213, 221, 0.04)',
-            borderColor: '#B0B5BD',
-          },
+          backgroundColor: "#FFFFFF",
+          borderColor: "#D0D5DD",
+          color: "#344054",
+          "&:hover": {
+            backgroundColor: "rgba(208, 213, 221, 0.04)",
+            borderColor: "#B0B5BD"
+          }
         },
         text: {
-          color: '#475467',
+          color: "#475467"
         },
         containedPrimary: {
-          color: '#FFFFFF',
+          color: "#FFFFFF"
         },
         containedSecondary: {
-          color: '#FFFFFF',
-        },
-      },
+          color: "#FFFFFF"
+        }
+      }
     },
     MuiCard: {
       styleOverrides: {
         root: {
-          borderWidth: '1px',
-          fontFamily: 'Poppins, sans-serif',
-          padding: '16px',
-        },
-      },
+          borderWidth: "1px",
+          fontFamily: "Poppins, sans-serif",
+          padding: "16px"
+        }
+      }
     },
     MuiCardActions: {
       styleOverrides: {
         root: {
-          padding: '0',
-        },
-      },
+          padding: "0"
+        }
+      }
     },
     MuiCardContent: {
       styleOverrides: {
         root: {
-          padding: '0',
-        },
-      },
+          padding: "0"
+        }
+      }
     },
     MuiCardHeader: {
       styleOverrides: {
         root: {
-          padding: '0',
-          marginBottom: '16px',
+          padding: "0",
+          marginBottom: "16px"
         },
         title: {
-          color: '#000000',
-          fontSize: '14px',
-          fontWeight: '600',
-          lineHeight: '20px',
-          textAlign: 'left',
-        },
-      },
+          color: "#000000",
+          fontSize: "14px",
+          fontWeight: "600",
+          lineHeight: "20px",
+          textAlign: "left"
+        }
+      }
     },
     MuiChip: {
       styleOverrides: {
         root: {
-          fontFamily: 'Poppins, sans-serif',
+          fontFamily: "Poppins, sans-serif",
           fontWeight: 500,
-          borderWidth: '1.10px',
-          '&.pending': {
-            backgroundColor: '#EFF8FF',
-            color: '#175CD3',
+          borderWidth: "1.10px",
+          "&.pending": {
+            backgroundColor: "#EFF8FF",
+            color: "#175CD3"
           },
-          '&.open': {
-            backgroundColor: '#F4F3FF',
-            color: '#5925DC',
+          "&.open": {
+            backgroundColor: "#F4F3FF",
+            color: "#5925DC"
           },
-          '&.approved': {
-            backgroundColor: '#ECFDF3',
-            color: '#027A48',
+          "&.approved": {
+            backgroundColor: "#ECFDF3",
+            color: "#027A48"
           },
-          '&.denied': {
-            backgroundColor: '#FEF3F2',
-            color: '#B42318',
+          "&.denied": {
+            backgroundColor: "#FEF3F2",
+            color: "#B42318"
           },
-          '&.enabled': {
-            borderColor: '#027A48',
-            color: '#027A48',
+          "&.enabled": {
+            borderColor: "#027A48",
+            color: "#027A48"
           },
-          '&.rejected': {
-            borderColor: '#D92D20',
-            color: '#D92D20',
+          "&.rejected": {
+            borderColor: "#D92D20",
+            color: "#D92D20"
           },
-          '&.disabled': {
-            borderColor: '#E7A63F',
-            color: '#E7A63F',
+          "&.disabled": {
+            borderColor: "#E7A63F",
+            color: "#E7A63F"
           },
-          '&.default': {
-            backgroundColor: '#EAECF0',
-            color: '#000000',
-          },
-        },
-      },
+          "&.default": {
+            backgroundColor: "#EAECF0",
+            color: "#000000"
+          }
+        }
+      }
     },
     MuiLink: {
       styleOverrides: {
         root: {
-          fontFamily: 'Poppins',
+          fontFamily: "Poppins",
           fontWeightMedium: 400,
-          fontSize: '16px',
-          lineHeight: '21px',
-        },
-      },
+          fontSize: "16px",
+          lineHeight: "21px"
+        }
+      }
     },
     MuiTypography: {
       styleOverrides: {
         root: {
-          fontWeight: 500,
+          fontWeight: 500
         },
         body1: {
           fontWeight: 400,
-          fontSize: '1rem',
-          lineHeight: '1.5rem',
+          fontSize: "1rem",
+          lineHeight: "1.5rem"
         },
         h1: {
-          fontSize: '2.25rem',
-          lineHeight: '2.75rem',
+          fontSize: "2.25rem",
+          lineHeight: "2.75rem"
         },
         h2: {
-          fontSize: '2rem',
-          lineHeight: '2.5rem',
+          fontSize: "2rem",
+          lineHeight: "2.5rem"
         },
         h3: {
-          fontSize: '1.5rem',
-          lineHeight: '2.25rem',
+          fontSize: "1.5rem",
+          lineHeight: "2.25rem"
         },
         h4: {
-          fontSize: '1.25rem',
-          lineHeight: '1.875rem',
+          fontSize: "1.25rem",
+          lineHeight: "1.875rem"
         },
         h5: {
-          fontSize: '1.125rem',
-          lineHeight: '1.75rem',
+          fontSize: "1.125rem",
+          lineHeight: "1.75rem"
         },
         h6: {
-          fontSize: '1rem',
-          lineHeight: '1.5rem',
-        },
-      },
-    },
-  },
+          fontSize: "1rem",
+          lineHeight: "1.5rem"
+        }
+      }
+    }
+  }
 });
 export default nextHrTheme;
