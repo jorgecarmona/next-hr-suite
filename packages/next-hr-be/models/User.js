b6ef117cb36565296e4e72e3310b5060a4dffba5
jorge.carmona@vertisystem.com <jorge.carmona@vertisystem.com>
2024-06-03 21:32:53 -0400

    feat: removed unnecessary fields for user

--- packages/next-hr-be/models/User.js
+++ packages/next-hr-be/models/User.js
@@ -1,16 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  lastName: {type: String, required: true},
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  phoneNumber: {type: String, required: true},
   username: {
     type: String,
     required: true,
-    unique: true
-  }
+    unique: true,
+  },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
