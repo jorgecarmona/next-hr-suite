681b712ebc20a87a0291035feb718398837da739
Nancy <nancy.blanco.suarez@gmail.com>
2024-06-06 18:56:48 -0500

    chore(landing): fixed landing test

--- packages/next-hr-fe/src/__tests__/pages/landing.test.tsx
+++ packages/next-hr-fe/src/__tests__/pages/landing.test.tsx
@@ -1,7 +1,8 @@
-import {render, screen, fireEvent} from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import Landing from '../../pages/landing';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import {useGetUsersQuery} from '../../store/api-slice/users-slice';
+
+import Landing from '../../pages/landing';
 
 jest.mock('../store/api-slice/users-slice', () => ({
   useGetUsersQuery: jest.fn(),
@@ -10,39 +11,34 @@ jest.mock('../store/api-slice/users-slice', () => ({
 const mockedUseGetUsersQuery = useGetUsersQuery as jest.Mock;
 
 describe('Landing Component', () => {
-  beforeEach(() => {
-    jest.spyOn(console, 'log').mockImplementation(() => {});
+  jest.spyOn(console, 'log').mockImplementation(() => {});
 
-    mockedUseGetUsersQuery.mockReturnValue({
-      data: [
-        {
-          id: 1,
-          username: 'user1',
-          email: 'user1@example.com',
-          phone: '123-456-7890',
-          website: 'user1.com',
-          company: {
-            name: 'Company1',
-            catchPhrase: 'Catch phrase 1',
-            bs: 'BS 1',
-          },
+  mockedUseGetUsersQuery.mockReturnValue({
+    data: [
+      {
+        id: 1,
+        username: 'user1',
+        email: 'user1@example.com',
+        phone: '123-456-7890',
+        website: 'user1.com',
+        company: {
+          name: 'Company1',
+          catchPhrase: 'Catch phrase 1',
+          bs: 'BS 1',
         },
-        // Add more users if needed
-      ],
-    });
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
+      },
+    ],
   });
 
   it('renders the main heading', () => {
     render(<Landing />);
+
     expect(screen.getByText('Hello! How can we help you?')).toBeInTheDocument();
   });
 
   it('renders the BigNavButtons with correct titles', () => {
     render(<Landing />);
+
     expect(screen.getByText('Submit')).toBeInTheDocument();
     expect(screen.getByText('Learn')).toBeInTheDocument();
     expect(screen.getByText('Review')).toBeInTheDocument();
@@ -51,17 +47,20 @@ describe('Landing Component', () => {
 
   it('BigNavButton click triggers handleChange', () => {
     render(<Landing />);
+
     const buttons = screen.getAllByRole('button');
 
-    buttons.forEach((button) => {
-      fireEvent.click(button);
+    buttons.forEach(async (button) => {
+      await userEvent.click(button);
       expect(console.log).toHaveBeenCalled();
     });
   });
 
   it('renders Avatar with correct props', () => {
     render(<Landing />);
-    const avatar = screen.getByAltText('avatar family');
+
+    const avatar = screen.getByText('avatar family');
+
     expect(avatar).toHaveAttribute('src', 'AvatarFriends');
     expect(avatar).toHaveAttribute('height', '357');
     expect(avatar).toHaveAttribute('width', '613');
@@ -69,7 +68,9 @@ describe('Landing Component', () => {
 
   it('useGetUsersQuery is called and data is logged', () => {
     render(<Landing />);
+
     expect(useGetUsersQuery).toHaveBeenCalled();
+
     expect(console.log).toHaveBeenCalledWith([
       {
         id: 1,
