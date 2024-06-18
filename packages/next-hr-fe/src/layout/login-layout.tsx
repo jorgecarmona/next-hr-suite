b4181ff83c5e77587f6b9a82e90f56befdf632c1
Sergio Gutiérrez <sergiogutierrez17080@gmail.com>
2024-06-18 16:54:13 -0500

    resolved comflicts

--- packages/next-hr-fe/src/layout/login-layout.tsx
+++ packages/next-hr-fe/src/layout/login-layout.tsx
@@ -69,12 +69,8 @@ function LoginLayout({children}: LoginLayoutProps) {
   };
 
   const containerRight = {
-<<<<<<< HEAD
-    backgroundColor: palette.info.light,
-=======
     backgroundColor: palette.custom.light,
     height: '100%',
->>>>>>> 819c858c0ea4e69d04847af16789df7e7bce44d3
     position: 'relative',
   };
 
@@ -84,12 +80,9 @@ function LoginLayout({children}: LoginLayoutProps) {
   };
 
   const containerLogin = {
-<<<<<<< HEAD
-=======
     backgroundColor: palette.custom.contrastText,
     boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
     height: '589px',
->>>>>>> 819c858c0ea4e69d04847af16789df7e7bce44d3
     left: '50%',
     top: '50%',
     position: 'absolute',
--- packages/next-hr-fe/src/molecules/index.ts
+++ packages/next-hr-fe/src/molecules/index.ts
@@ -3,8 +3,4 @@ import ButtonsToolbar from './buttons-toolbar';
 import HeaderToolbar from './header-toolbar';
 import NavItem from './navItem';
 
-<<<<<<< HEAD
-export {BigNavButton, ButtonsToolbar, NavItem};
-=======
 export {BigNavButton, ButtonsToolbar, HeaderToolbar, NavItem};
->>>>>>> 819c858c0ea4e69d04847af16789df7e7bce44d3
