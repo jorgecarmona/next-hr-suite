0c080dd5f4e25f52f16bd4bb5b7d89ee4a1c7de9
Nancy <nancy.blanco.suarez@gmail.com>
2024-07-10 15:36:52 -0500

    feat: added index

--- packages/next-hr-fe/src/pages/index.ts
+++ packages/next-hr-fe/src/pages/index.ts
@@ -1,6 +1,7 @@
 import AccountSignUp from './accountsignup';
 import ApiPlayground from './api-playground';
+import Landing from './landing';
 import UserLogin from './userlogin';
 import PlayGround from './playground';
 
-export {AccountSignUp, ApiPlayground, PlayGround, UserLogin};
+export {AccountSignUp, ApiPlayground, Landing, PlayGround, UserLogin};
