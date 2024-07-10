09c0164be90fb698f2c89ee1d87b19e43121ae9a
Nancy <nancy.blanco.suarez@gmail.com>
2024-07-10 14:23:13 -0500

    updated index

--- packages/next-hr-fe/src/pages/index.ts
+++ packages/next-hr-fe/src/pages/index.ts
@@ -1,5 +1,6 @@
 import ApiPlayground from './api-playground';
+import Landing from './landing';
 import UserLogin from './userlogin';
 import PlayGround from './playground';
 
-export {ApiPlayground, PlayGround, UserLogin};
+export {ApiPlayground, Landing, PlayGround, UserLogin};
