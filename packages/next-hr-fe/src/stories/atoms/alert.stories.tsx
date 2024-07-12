a78c05450b43ec3d702c43cea99448d87e5d4649
Ingrid Julieth <injuvillalba10@gmail.com>
2024-07-12 14:39:47 -0500

    fix(alert): changes required

--- packages/next-hr-fe/src/stories/atoms/alert.stories.tsx
+++ packages/next-hr-fe/src/stories/atoms/alert.stories.tsx
@@ -1,28 +1,58 @@
 import {Meta, StoryObj} from '@storybook/react';
 
 import Alert from '../../atoms/alert';
+import {action} from '@storybook/addon-actions';
 
-const meta: Meta = {
+type AlertProps = {
+  type: 'error' | 'info' | 'success' | 'warning';
+  children: React.ReactNode;
+};
+
+const meta: Meta<AlertProps> = {
   title: 'Atoms/Alert',
   component: Alert,
   tags: ['autodocs'],
   parameters: {
     layout: 'centered',
   },
+  argTypes: {
+    type: {
+      control: 'select',
+      options: ['error', 'info', 'success', 'warning'],
+    },
+  },
 };
 
 export default meta;
 
-export const Error = () => (
-  <Alert severity="error">This is an error alert</Alert>
-);
+export const Error = {
+  args: {
+    type: 'error',
+    children: 'This is an error alert',
+    onClose: action('onClose was called'),
+  },
+};
 
-export const Warning = () => (
-  <Alert severity="warning">This is a warning alert</Alert>
-);
+export const Info = {
+  args: {
+    type: 'info',
+    children: 'This is an info alert',
+    onClose: action('onClose was called'),
+  },
+};
 
-export const Info = () => <Alert severity="info">This is an info alert</Alert>;
+export const Success = {
+  args: {
+    type: 'success',
+    children: 'This is an success alert',
+    onClose: action('onClose was called'),
+  },
+};
 
-export const Success = () => (
-  <Alert severity="success">This is a success alert</Alert>
-);
+export const Warning = {
+  args: {
+    type: 'warning',
+    children: 'This is an warning alert',
+    onClose: action('onClose was called'),
+  },
+};
